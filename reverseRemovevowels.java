46e0dce9a816b359db65515d8bfb934593ee229f
Mathimahesh <36835393+Mathimahesh@users.noreply.github.com>
2019-03-11 19:37:34 +0530

    Update reverseRemovevowels.java

--- reverseRemovevowels.java
+++ reverseRemovevowels.java
@@ -2,9 +2,10 @@ import java.util.*;
 public class MyClass {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         String s = sc.next();
         String ans = "";
-        for(int i=s.length()-1;i>=0;i--){
+        for(int i=n-1;i>=0;i--){
             ans+=s.charAt(i);
         }
         String val = ans.replaceAll("([aAeEiIoOuU])","");
