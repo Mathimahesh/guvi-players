b68183dd947ba8cbd02aeefb5c9a089c9bdf19ed
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-06-10 11:54:33 +0530

    Create primeRange.java

---
+++ primeRange.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int flag = 0;
+        for(int i=a;i<=b;i++){
+            flag=0;
+            for(int j =2;j<i;j++){
+                if(i%j==0){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag!=1){
+                System.out.println(i);
+            }
+        }
+    }
+}
