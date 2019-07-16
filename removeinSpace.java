9de463e677c7b1638f222257c2709c4e3f297d45
MaheshwariS <36835393+Mathimahesh@users.noreply.github.com>
2019-07-16 11:30:00 +0530

    Create removeinSpace.java

---
+++ removeinSpace.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String s = str.replaceAll(" ","");
+        System.out.println(s);
+    }
+}
